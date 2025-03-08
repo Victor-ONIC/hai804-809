641d64370a0de7bfe247bb1c612263548d9d266b
Clément Sapérès <clement.saperes@etu.umontpellier.fr>
2025-03-08 22:10:34 +0100

    ajustement

--- test/test.py
+++ test/test.py
@@ -3,10 +3,10 @@ from Histogramme import Histogramme
 
 nameImIn = "perso.ppm"
 imIn = Image.open(nameImIn)
+hist = Histogramme(16)
+hist.load(imIn)
 nbx = imIn.size[0]
 nby = imIn.size[1]
 total = nbx*nby
-hist = Histogramme(16)
-hist.load(imIn)
 for i in range(16) :
     print(i, round(hist.data[i]/float(total)*100, 2), "%")
