6345d9d62ae2a7e913ff515a47e566b2bd239967
clement.saperes@etu.umontpellier.fr <clement.saperes@etu.umontpellier.fr>
2025-03-24 16:32:28 +0100

    voisinage donne un résultat mais à pofiner

--- src/utils/Voisinage.py
+++ src/utils/Voisinage.py
@@ -1,7 +1,27 @@
 from colorsys import rgb_to_hsv as r2h
 from PIL import Image
 from Modele import Modele
+        
+def voisinsSup(i, j, nbx, nby) :
+    v = []
+    if (i < nbx-1) :
+        v.append([i+1, j])
+    if (j < nby-1) :
+        v.append([i, j+1])
+    return v
 
+def voisins(i, j, nbx, nby) :
+    v = []
+    if (i > 0) :
+        v.append([i-1, j])
+    if (i < nbx-1) :
+        v.append([i+1, j])
+    if (j > 0) :
+        v.append([i, j-1])
+    if (j < nby-1) :
+        v.append([i, j+1])
+    return v
+    
 class Voisinage :
     def __init__(self, image, seuil) :
         self.seuil = seuil
@@ -20,10 +40,10 @@ class Voisinage :
             for j in range(nby) :
                 if (self.data[j*nbx+i] == 0) :
                     self.data[j*nbx+i] = value
-                    voisin = self.voisinsSup(i, j, nbx, nby)
+                    voisin = voisinsSup(i, j, nbx, nby)
                     for v in voisin :
                         if (self.data[v[1]*nbx+v[0]] == 0) :
-                            voisin_local = self.voisins([v[0], v[1], nbx, nby])
+                            voisin_local = voisins(v[0], v[1], nbx, nby)
                             for vl in voisin_local :
                                 if (self.data[vl[1]*nbx+vl[0]] == value and Modele.distance_congru(hues[vl[1]*nbx+vl[0]], hues[v[1]*nbx+v[0]]) < self.seuil) :
                                     self.data[v[1]*nbx+v[0]] = value
@@ -32,8 +52,8 @@ class Voisinage :
                                     if (self.data[vl[1]*nbx+vl[0]] == 0) :
                                         voisin.append(vl)
                 value += 1
-        
-        def voisinsSup(self, i, j, nbx, nby) :
+        """
+        def voisinsSup(i, j, nbx, nby) :
             v = []
             if (i < nbx-1) :
                 v.append([i+1, j])
@@ -41,7 +61,7 @@ class Voisinage :
                 v.append([i, j+1])
             return v
         
-        def voisins(self, i, j, nbx, nby) :
+        def voisins(i, j, nbx, nby) :
             v = []
             if (i > 0) :
                 v.append([i-1, j])
@@ -51,15 +71,26 @@ class Voisinage :
                 v.append([i, j-1])
             if (j < nby-1) :
                 v.append([i, j+1])
-            return v
+            return v"""
 
-nameImIn = "hai804-809\src\images\perso.ppm"
+nameImIn = "../images/colorful.ppm"
 imIn = Image.open(nameImIn)
-vois = Voisinage(imIn, 0.1)
+vois = Voisinage(imIn, 0.04)
 nbx = imIn.size[0]
 nby = imIn.size[1]
+dataIn = imIn.load()
+imOut = Image.new(imIn.mode, imIn.size)
+dataOut = imOut.load()
+vData = vois.data
+v=0
+for i in vData :
+    if (i>v) :
+        v=i
 for i in range(nbx) :
     for j in range(nby) :
-        print(vois.data[j*nbx+i], endl="")
-    print()
-print()
+        r, g, b = [int(255*vData[j*nbx+i]/v) for c in range(3)]
+        dataOut[i, j] = (r, g, b)
+print(v)
+imIn.close()
+imOut.save("../images/test_voisinage.ppm")
+imOut.close()
