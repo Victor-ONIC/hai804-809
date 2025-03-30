c68ae479abd7662b5c0814199f49a2ca0468dd96
Clément Sapérès <clement.saperes@etu.umontpellier.fr>
2025-03-30 19:43:45 +0200

    harmonization 2 (partie 2 en cours)

--- src/Harmonization_JIANCHAO TAN.py
+++ src/Harmonization_JIANCHAO TAN.py
@@ -1,3 +1,4 @@
+import math
 from PIL import Image
 import scipy.spatial as ss #import ConvexHull
 import numpy as np
@@ -14,14 +15,49 @@ def loadData(imIn) :
     npdata = np.array(ldata)
     return npdata
 
-def harmonization(imIn) :
+def distance(v1, v2) :
+    res = 0
+    for i in range(len(v1)) :
+        res += (v1[i]-v2[i])**2
+    return math.sqrt(res)
+
+def moyenne(vertices) :
+    res = [0 for i in range(len(vertices[0]))]
+    for vertex in vertices :
+        for i in range(len(vertex)) :
+            res[i] += vertex[i]
+    for i in range(len(res)) :
+        res[i] /= len(vertices)
+    return res
+
+def reducHull(vertices, targetNbr) :
+    currentNbr = len(vertices)
+    verticesPacks = [[i] for i in vertices]
+    newVertices = [i for i in vertices]
+    while (currentNbr > targetNbr) :
+        min = distance(vertices[0], vertices[1])
+        index = [0, 1]
+        for i in range(currentNbr-1) :
+            for j in range(i+1, currentNbr) :
+                d = distance(newVertices[i], newVertices[j])
+                if (d < min) :
+                    min = d
+                    index = [i, j]
+        verticesPacks[index[0]] += verticesPacks.pop(index[1])
+        newVertices[index[0]] = moyenne(verticesPacks[index[0]])
+        newVertices.pop(index[1])
+        currentNbr -= 1
+    return newVertices
+
+def harmonization(imIn, nbColor) :
     #0 Récupérer les données de l'image
     data = loadData(imIn)
     #1 calcul de l'enveloppe convexe RGB
     RGB_conv = ss.ConvexHull(data)
     RGB_vertices = data[RGB_conv.vertices]
-    #1.5 réduire le nombre de point de l'enveloppe en fonction d'un nombre arbitraire de couleur (en fait c'est le point numéro 2)
     #2 calcul de la palette
+    RGB_reduc_conv = reducHull(RGB_vertices, nbColor)
+    #2.5 étirer les points pour réenvelopper ce qui est sorti
     #3 calcul de l'enveloppe convexe RGBXY
     #4 calcul des coordonnées barycentrique RGB et RGBXY
     #5 multiplication des matrices de coordonnées pour avoir la proportion de chaque couleur de la palette 
@@ -29,4 +65,4 @@ def harmonization(imIn) :
 
 nameImIn = "images\colorful.ppm"
 imIn = Image.open(nameImIn)
-harmonization(imIn)
+harmonization(imIn, 8)
